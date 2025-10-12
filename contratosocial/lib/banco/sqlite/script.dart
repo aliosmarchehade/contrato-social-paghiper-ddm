5cc0da22398a1c4564613e8f1d3609807706cbf2
Claudir Fantuci Junior <cladirfantcijunior@gmail.com>
2025-10-12 18:01:29 -0300

    feat | dashboard pegando dados do banco e dados iniciais cadastrados

--- contratosocial/lib/banco/sqlite/script.dart
+++ contratosocial/lib/banco/sqlite/script.dart
@@ -103,5 +103,232 @@ class ScriptSQLite {
       VALUES ('admin', 'admin@sistema.com', 'admin123')
       ''',
     ],
+    [
+      // Contract 1
+      '''
+      INSERT INTO endereco (logradouro, numero, complemento, bairro, cidade, estado, cep)
+      VALUES ('Rua das Flores', '100', 'Apto 101', 'Centro', 'São Paulo', 'SP', '01001-000')
+      ''',
+      '''
+      INSERT INTO empresa (nome_empresarial, cnpj, endereco_id, objeto_social, duracao_sociedade)
+      VALUES ('Tech Solutions LTDA', '12.345.678/0001-99', 1, 'Desenvolvimento de software', 'Indeterminada')
+      ''',
+      '''
+      INSERT INTO capital_social (valor_total, forma_integralizacao, prazo_integralizacao)
+      VALUES (100000.00, 'Dinheiro', '12 meses')
+      ''',
+      '''
+      INSERT INTO administracao (tipo_administracao, regras)
+      VALUES ('Diretoria', 'Decisões por maioria')
+      ''',
+      '''
+      INSERT INTO duracao_exercicio_social (periodo, data_inicio, data_fim)
+      VALUES ('Anual', '2025-01-01', '2025-12-31')
+      ''',
+      '''
+      INSERT INTO contrato_social (data_upload, data_processamento, empresa_id, administracao_id, capital_social_id, duracao_exercicio_id)
+      VALUES ('2025-10-01T10:00:00Z', '2025-10-02T10:00:00Z', 1, 1, 1, 1)
+      ''',
+      '''
+      INSERT INTO socio (nome, documento, endereco_id, profissao, percentual, tipo, nacionalidade, estado_civil, contrato_social_id)
+      VALUES ('João Silva', '123.456.789-00', 1, 'Engenheiro', 60.0, 'Administrador', 'Brasileiro', 'Casado', 1)
+      ''',
+      '''
+      INSERT INTO socio (nome, documento, endereco_id, profissao, percentual, tipo, nacionalidade, estado_civil, contrato_social_id)
+      VALUES ('Maria Santos', '987.654.321-00', 1, 'Advogada', 40.0, 'Investidor', 'Brasileiro', 'Solteiro', 1)
+      ''',
+      '''
+      INSERT INTO clausulas (titulo, descricao, contrato_social_id)
+      VALUES ('Cláusula 1', 'Definição do objeto social', 1)
+      ''',
+      '''
+      INSERT INTO clausulas (titulo, descricao, contrato_social_id)
+      VALUES ('Cláusula 2', 'Distribuição de lucros', 1)
+      ''',
+    ],
+    [
+      // Contract 2
+      '''
+      INSERT INTO endereco (logradouro, numero, complemento, bairro, cidade, estado, cep)
+      VALUES ('Avenida Brasil', '200', '', 'Jardins', 'Rio de Janeiro', 'RJ', '20040-000')
+      ''',
+      '''
+      INSERT INTO empresa (nome_empresarial, cnpj, endereco_id, objeto_social, duracao_sociedade)
+      VALUES ('Comércio Verde LTDA', '23.456.789/0001-88', 2, 'Venda de produtos orgânicos', '20 anos')
+      ''',
+      '''
+      INSERT INTO capital_social (valor_total, forma_integralizacao, prazo_integralizacao)
+      VALUES (50000.00, 'Bens móveis', '6 meses')
+      ''',
+      '''
+      INSERT INTO administracao (tipo_administracao, regras)
+      VALUES ('Conselho', 'Decisões unânimes')
+      ''',
+      '''
+      INSERT INTO duracao_exercicio_social (periodo, data_inicio, data_fim)
+      VALUES ('Anual', '2025-01-01', '2025-12-31')
+      ''',
+      '''
+      INSERT INTO contrato_social (data_upload, data_processamento, empresa_id, administracao_id, capital_social_id, duracao_exercicio_id)
+      VALUES ('2025-10-02T12:00:00Z', '2025-10-03T12:00:00Z', 2, 2, 2, 2)
+      ''',
+      '''
+      INSERT INTO socio (nome, documento, endereco_id, profissao, percentual, tipo, nacionalidade, estado_civil, contrato_social_id)
+      VALUES ('Ana Costa', '111.222.333-44', 2, 'Comerciante', 50.0, 'Administrador', 'Brasileiro', 'Casado', 2)
+      ''',
+      '''
+      INSERT INTO socio (nome, documento, endereco_id, profissao, percentual, tipo, nacionalidade, estado_civil, contrato_social_id)
+      VALUES ('Pedro Almeida', '555.666.777-88', 2, 'Contador', 50.0, 'Investidor', 'Brasileiro', 'Solteiro', 2)
+      ''',
+      '''
+      INSERT INTO clausulas (titulo, descricao, contrato_social_id)
+      VALUES ('Cláusula 1', 'Definição do objeto social', 2)
+      ''',
+      '''
+      INSERT INTO clausulas (titulo, descricao, contrato_social_id)
+      VALUES ('Cláusula 2', 'Regras de administração', 2)
+      ''',
+    ],
+    [
+      // Contract 3
+      '''
+      INSERT INTO endereco (logradouro, numero, complemento, bairro, cidade, estado, cep)
+      VALUES ('Rua do Comércio', '300', 'Sala 10', 'Bela Vista', 'Belo Horizonte', 'MG', '30112-000')
+      ''',
+      '''
+      INSERT INTO empresa (nome_empresarial, cnpj, endereco_id, objeto_social, duracao_sociedade)
+      VALUES ('Inova Tech LTDA', '34.567.890/0001-77', 3, 'Consultoria em TI', 'Indeterminada')
+      ''',
+      '''
+      INSERT INTO capital_social (valor_total, forma_integralizacao, prazo_integralizacao)
+      VALUES (200000.00, 'Dinheiro', '24 meses')
+      ''',
+      '''
+      INSERT INTO administracao (tipo_administracao, regras)
+      VALUES ('Diretoria', 'Decisões por maioria')
+      ''',
+      '''
+      INSERT INTO duracao_exercicio_social (periodo, data_inicio, data_fim)
+      VALUES ('Anual', '2025-01-01', '2025-12-31')
+      ''',
+      '''
+      INSERT INTO contrato_social (data_upload, data_processamento, empresa_id, administracao_id, capital_social_id, duracao_exercicio_id)
+      VALUES ('2025-10-03T14:00:00Z', '2025-10-04T14:00:00Z', 3, 3, 3, 3)
+      ''',
+      '''
+      INSERT INTO socio (nome, documento, endereco_id, profissao, percentual, tipo, nacionalidade, estado_civil, contrato_social_id)
+      VALUES ('Lucas Ferreira', '222.333.444-55', 3, 'Programador', 70.0, 'Administrador', 'Brasileiro', 'Solteiro', 3)
+      ''',
+      '''
+      INSERT INTO socio (nome, documento, endereco_id, profissao, percentual, tipo, nacionalidade, estado_civil, contrato_social_id)
+      VALUES ('Clara Mendes', '666.777.888-99', 3, 'Designer', 30.0, 'Investidor', 'Brasileiro', 'Casado', 3)
+      ''',
+      '''
+      INSERT INTO clausulas (titulo, descricao, contrato_social_id)
+      VALUES ('Cláusula 1', 'Definição do objeto social', 3)
+      ''',
+      '''
+      INSERT INTO clausulas (titulo, descricao, contrato_social_id)
+      VALUES ('Cláusula 2', 'Distribuição de lucros', 3)
+      ''',
+      '''
+      INSERT INTO clausulas (titulo, descricao, contrato_social_id)
+      VALUES ('Cláusula 3', 'Resolução de conflitos', 3)
+      ''',
+    ],
+    [
+      // Contract 4
+      '''
+      INSERT INTO endereco (logradouro, numero, complemento, bairro, cidade, estado, cep)
+      VALUES ('Rua das Palmeiras', '400', '', 'Boa Viagem', 'Recife', 'PE', '51020-000')
+      ''',
+      '''
+      INSERT INTO empresa (nome_empresarial, cnpj, endereco_id, objeto_social, duracao_sociedade)
+      VALUES ('Nordeste Turismo LTDA', '45.678.901/0001-66', 4, 'Agência de turismo', '15 anos')
+      ''',
+      '''
+      INSERT INTO capital_social (valor_total, forma_integralizacao, prazo_integralizacao)
+      VALUES (80000.00, 'Dinheiro', '18 meses')
+      ''',
+      '''
+      INSERT INTO administracao (tipo_administracao, regras)
+      VALUES ('Conselho', 'Decisões por maioria')
+      ''',
+      '''
+      INSERT INTO duracao_exercicio_social (periodo, data_inicio, data_fim)
+      VALUES ('Anual', '2025-01-01', '2025-12-31')
+      ''',
+      '''
+      INSERT INTO contrato_social (data_upload, data_processamento, empresa_id, administracao_id, capital_social_id, duracao_exercicio_id)
+      VALUES ('2025-10-04T16:00:00Z', '2025-10-05T16:00:00Z', 4, 4, 4, 4)
+      ''',
+      '''
+      INSERT INTO socio (nome, documento, endereco_id, profissao, percentual, tipo, nacionalidade, estado_civil, contrato_social_id)
+      VALUES ('Rafael Lima', '333.444.555-66', 4, 'Guia Turístico', 50.0, 'Administrador', 'Brasileiro', 'Casado', 4)
+      ''',
+      '''
+      INSERT INTO socio (nome, documento, endereco_id, profissao, percentual, tipo, nacionalidade, estado_civil, contrato_social_id)
+      VALUES ('Sofia Ribeiro', '777.888.999-00', 4, 'Administradora', 30.0, 'Investidor', 'Brasileiro', 'Solteiro', 4)
+      ''',
+      '''
+      INSERT INTO socio (nome, documento, endereco_id, profissao, percentual, tipo, nacionalidade, estado_civil, contrato_social_id)
+      VALUES ('Tiago Souza', '888.999.000-11', 4, 'Contador', 20.0, 'Investidor', 'Brasileiro', 'Casado', 4)
+      ''',
+      '''
+      INSERT INTO clausulas (titulo, descricao, contrato_social_id)
+      VALUES ('Cláusula 1', 'Definição do objeto social', 4)
+      ''',
+      '''
+      INSERT INTO clausulas (titulo, descricao, contrato_social_id)
+      VALUES ('Cláusula 2', 'Regras de administração', 4)
+      ''',
+    ],
+    [
+      // Contract 5
+      '''
+      INSERT INTO endereco (logradouro, numero, complemento, bairro, cidade, estado, cep)
+      VALUES ('Avenida Central', '500', 'Bloco A', 'Asa Sul', 'Brasília', 'DF', '70350-000')
+      ''',
+      '''
+      INSERT INTO empresa (nome_empresarial, cnpj, endereco_id, objeto_social, duracao_sociedade)
+      VALUES ('Consultoria Nacional LTDA', '56.789.012/0001-55', 5, 'Consultoria empresarial', 'Indeterminada')
+      ''',
+      '''
+      INSERT INTO capital_social (valor_total, forma_integralizacao, prazo_integralizacao)
+      VALUES (150000.00, 'Dinheiro', '12 meses')
+      ''',
+      '''
+      INSERT INTO administracao (tipo_administracao, regras)
+      VALUES ('Diretoria', 'Decisões por maioria')
+      ''',
+      '''
+      INSERT INTO duracao_exercicio_social (periodo, data_inicio, data_fim)
+      VALUES ('Anual', '2025-01-01', '2025-12-31')
+      ''',
+      '''
+      INSERT INTO contrato_social (data_upload, data_processamento, empresa_id, administracao_id, capital_social_id, duracao_exercicio_id)
+      VALUES ('2025-10-05T18:00:00Z', '2025-10-06T18:00:00Z', 5, 5, 5, 5)
+      ''',
+      '''
+      INSERT INTO socio (nome, documento, endereco_id, profissao, percentual, tipo, nacionalidade, estado_civil, contrato_social_id)
+      VALUES ('Beatriz Oliveira', '444.555.666-77', 5, 'Consultora', 60.0, 'Administrador', 'Brasileiro', 'Solteiro', 5)
+      ''',
+      '''
+      INSERT INTO socio (nome, documento, endereco_id, profissao, percentual, tipo, nacionalidade, estado_civil, contrato_social_id)
+      VALUES ('Gustavo Pereira', '999.000.111-22', 5, 'Economista', 40.0, 'Investidor', 'Brasileiro', 'Casado', 5)
+      ''',
+      '''
+      INSERT INTO clausulas (titulo, descricao, contrato_social_id)
+      VALUES ('Cláusula 1', 'Definição do objeto social', 5)
+      ''',
+      '''
+      INSERT INTO clausulas (titulo, descricao, contrato_social_id)
+      VALUES ('Cláusula 2', 'Distribuição de lucros', 5)
+      ''',
+      '''
+      INSERT INTO clausulas (titulo, descricao, contrato_social_id)
+      VALUES ('Cláusula 3', 'Resolução de conflitos', 5)
+      ''',
+    ],
   ];
 }
--- contratosocial/lib/telas/dashboard/dashboard.dart
+++ contratosocial/lib/telas/dashboard/dashboard.dart
@@ -1,35 +1,80 @@
-import 'package:contratosocial/models/usuario.dart';
-import 'package:flutter/material.dart';
+import 'package:contratosocial/banco/sqlite/dao/contrato_social_dao.dart';
+import 'package:contratosocial/banco/sqlite/dao/empresa_dao.dart';
+import 'package:contratosocial/banco/sqlite/dao/socio_dao.dart';
 import 'package:contratosocial/configuracao/rotas.dart';
 import 'package:contratosocial/components/app_drawer.dart';
+import 'package:contratosocial/models/contrato_social.dart';
+import 'package:contratosocial/models/empresa.dart';
+import 'package:contratosocial/models/socio.dart';
+import 'package:flutter/material.dart';
 
-
-class Dashboard extends StatelessWidget {
-
-  // final Usuario usuario;
-
-  // const Dashboard({super.key, required this.usuario});
+class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // Mock de dados (poderia vir de DAO/Provider)
-    int contratosAnalisados = 12;
-    int sociosCadastrados = 35;
+  State<Dashboard> createState() => _DashboardState();
+}
 
-    Map<String, dynamic> ultimoContrato = {
-      "data_analise": "02/10/2025",
-      "data_atualizacao": "05/10/2025",
-      "empresa": {
-        "nome_empresarial": "Tech Solutions LTDA",
-        "cnpj": "12.345.678/0001-99",
-      },
-      "socios": [
-        {"nome": "Maria Silva", "documento": "123.456.789-00"},
-        {"nome": "João Santos", "documento": "987.654.321-00"},
-      ],
+class _DashboardState extends State<Dashboard> {
+  late Future<Map<String, dynamic>> _dashboardDataFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _dashboardDataFuture = _carregarDadosDashboard();
+  }
+
+  Future<Map<String, dynamic>> _carregarDadosDashboard() async {
+    final contratoDao = DAOContratoSocial();
+    final socioDao = DAOSocio();
+    final empresaDao = DAOEmpresa();
+
+    // Fetch total number of contracts
+    final contratos = await contratoDao.buscarTodos();
+    final contratosAnalisados = contratos.length;
+
+    // Fetch total number of partners
+    final socios = await socioDao.buscarTodos();
+    final sociosCadastrados = socios.length;
+
+    // Fetch the most recent contract
+    Map<String, dynamic> ultimoContrato = {};
+    if (contratos.isNotEmpty) {
+      // Sort contracts by data_upload in descending order to get the most recent
+      contratos.sort((a, b) => b.dataUpload.compareTo(a.dataUpload));
+      final contrato = contratos.first;
+      final empresa = await empresaDao.buscarPorId(contrato.empresaId);
+      final sociosContrato = await socioDao.buscarPorContratoSocial(
+        contrato.id!,
+      );
+
+      if (empresa != null) {
+        ultimoContrato = {
+          'data_analise':
+              contrato.dataProcessamento.toIso8601String().split('T')[0],
+          'data_atualizacao':
+              contrato.dataUpload.toIso8601String().split('T')[0],
+          'empresa': {
+            'nome_empresarial': empresa.nomeEmpresarial,
+            'cnpj': empresa.cnpj,
+          },
+          'socios':
+              sociosContrato
+                  .map((s) => {'nome': s.nome, 'documento': s.documento})
+                  .toList(),
+        };
+      }
+    }
+
+    return {
+      'contratosAnalisados': contratosAnalisados,
+      'sociosCadastrados': sociosCadastrados,
+      'ultimoContrato': ultimoContrato,
     };
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -38,151 +83,174 @@ class Dashboard extends StatelessWidget {
         backgroundColor: const Color(0xFF0860DB),
         iconTheme: const IconThemeData(color: Colors.white),
         title: const Text(
-          "Contrato Social - Dashboard  ",
+          "Contrato Social - Dashboard",
           style: TextStyle(color: Colors.white),
         ),
       ),
       drawer: const AppDrawer(),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Linha com os 2 cards
-            Row(
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: _dashboardDataFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Erro ao carregar dados: ${snapshot.error}'),
+            );
+          } else if (!snapshot.hasData ||
+              snapshot.data!['ultimoContrato'].isEmpty) {
+            return const Center(
+              child: Text('Nenhum contrato salvo encontrado.'),
+            );
+          }
+
+          final data = snapshot.data!;
+          final contratosAnalisados = data['contratosAnalisados'] as int;
+          final sociosCadastrados = data['sociosCadastrados'] as int;
+          final ultimoContrato = data['ultimoContrato'] as Map<String, dynamic>;
+
+          return SingleChildScrollView(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Expanded(
-                  child: Card(
-                    color: Color.fromARGB(255, 194, 213, 241),
-                    elevation: 5,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Column(
-                        children: [
-                          const Icon(
-                            Icons.description,
-                            color: Colors.blue,
-                            size: 40,
-                          ),
-                          const SizedBox(height: 10),
-                          const Text(
-                            "Contratos analisados",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            "$contratosAnalisados",
-                            style: const TextStyle(
-                              fontSize: 22,
-                              color: Colors.black87,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 12),
-                Expanded(
-                  child: Card(
-                    color: Color.fromARGB(255, 194, 213, 241),
-                    elevation: 5,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Column(
-                        children: [
-                          const Icon(
-                            Icons.people,
-                            color: Colors.blue,
-                            size: 40,
-                          ),
-                          const SizedBox(height: 10),
-                          const Text(
-                            "Sócios cadastrados",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            "$sociosCadastrados",
-                            style: const TextStyle(
-                              fontSize: 22,
-                              color: Colors.black87,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-
-            const SizedBox(height: 30),
-
-            // Resumo último contrato
-            const Text(
-              "Último contrato analisado",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            const Divider(),
-            Card(
-              color: Color.fromARGB(255, 194, 213, 241),
-              elevation: 5,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                // Linha com os 2 cards
+                Row(
                   children: [
-                    Text(
-                      "Empresa: ${ultimoContrato['empresa']['nome_empresarial']}",
-                      style: const TextStyle(fontWeight: FontWeight.w500),
-                    ),
-                    Text("CNPJ: ${ultimoContrato['empresa']['cnpj']}"),
-                    const SizedBox(height: 10),
-                    Text("Data de análise: ${ultimoContrato['data_analise']}"),
-                    Text(
-                      "Última atualização: ${ultimoContrato['data_atualizacao']}",
-                    ),
-                    const SizedBox(height: 10),
-                    const Text(
-                      "Sócios:",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    ...ultimoContrato['socios'].map<Widget>(
-                      (s) => Text("- ${s['nome']} (${s['documento']})"),
-                    ),
-                    const SizedBox(height: 12),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blueAccent,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
+                    Expanded(
+                      child: Card(
+                        color: const Color.fromARGB(255, 194, 213, 241),
+                        elevation: 5,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Column(
+                            children: [
+                              const Icon(
+                                Icons.description,
+                                color: Colors.blue,
+                                size: 40,
+                              ),
+                              const SizedBox(height: 10),
+                              const Text(
+                                "Contratos analisados",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                "$contratosAnalisados",
+                                style: const TextStyle(
+                                  fontSize: 22,
+                                  color: Colors.black87,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                        onPressed: () {
-                          _showContractDialog(context, ultimoContrato);
-                        },
-                        child: const Text(
-                          "Ver detalhes",
-                          style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    const SizedBox(width: 12),
+                    Expanded(
+                      child: Card(
+                        color: const Color.fromARGB(255, 194, 213, 241),
+                        elevation: 5,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Column(
+                            children: [
+                              const Icon(
+                                Icons.people,
+                                color: Colors.blue,
+                                size: 40,
+                              ),
+                              const SizedBox(height: 10),
+                              const Text(
+                                "Sócios cadastrados",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                "$sociosCadastrados",
+                                style: const TextStyle(
+                                  fontSize: 22,
+                                  color: Colors.black87,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
                   ],
                 ),
-              ),
+                const SizedBox(height: 30),
+                // Resumo último contrato
+                const Text(
+                  "Último contrato analisado",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const Divider(),
+                Card(
+                  color: const Color.fromARGB(255, 194, 213, 241),
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Empresa: ${ultimoContrato['empresa']['nome_empresarial']}",
+                          style: const TextStyle(fontWeight: FontWeight.w500),
+                        ),
+                        Text("CNPJ: ${ultimoContrato['empresa']['cnpj']}"),
+                        const SizedBox(height: 10),
+                        Text(
+                          "Data de análise: ${ultimoContrato['data_analise']}",
+                        ),
+                        Text(
+                          "Última atualização: ${ultimoContrato['data_atualizacao']}",
+                        ),
+                        const SizedBox(height: 10),
+                        const Text(
+                          "Sócios:",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        ...ultimoContrato['socios'].map<Widget>(
+                          (s) => Text("- ${s['nome']} (${s['documento']})"),
+                        ),
+                        const SizedBox(height: 12),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blueAccent,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                            onPressed: () {
+                              _showContractDialog(context, ultimoContrato);
+                            },
+                            child: const Text(
+                              "Ver detalhes",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
